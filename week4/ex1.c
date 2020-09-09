99e350ef1dfec4263cb44fe3f3ec8ac3d18548eb
Truong Nguyen <t.nguyen@innopolis.university>
2020-09-09 10:31:17 +0300

    Assignment week 4 OS Truong Nguyen

---
+++ week4/ex1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<unistd.h>
+int main(void){
+	int n = 10;
+	pid_t pid = fork();
+	for(int i = 0; i < n ; i++){
+	if(pid == 0)
+		printf("Hello from child [%d - %d]\n",pid,i);
+	else if(pid > 0)
+		printf("Hello from parent [%d - %d]\n", pid,i);
+	else
+		return -1;
+	}
+	return 0;
+}
+//Out put will be random sequence of hello's from parent and chil, because the CPU randomly excecute them.
